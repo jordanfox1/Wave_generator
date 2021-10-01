259d053e058eb75e64cdb43b6012d9bdf8425417
jordanfox1 <jordanfox9415@gmail.com>
2021-10-01 21:38:54 +1000

    updated app usage, no longer available as a gem

--- lib/wave_generator.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "wave_generator/version"
-require_relative "wave_generator/main"
-
-module WaveGenerator
-  class Error < StandardError; end
-  require 'wave_generator'
-end
