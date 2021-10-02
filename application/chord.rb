5c7e69c14435990ff46654c6302c471f76fcd0a2
jordanfox1 <jordanfox9415@gmail.com>
2021-10-02 13:39:35 +1000

    testing features

--- application/chord.rb
+++ application/chord.rb
@@ -5,7 +5,7 @@ require_relative 'methods'
 include Notes::UserInterface
 
 puts Rainbow("What do you want to name your file?").blue
-OUTPUT_FILENAME = "../../files/" + STDIN.gets.chomp + '.wav'
+OUTPUT_FILENAME = "./files/" + STDIN.gets.chomp + '.wav'
 
 # This is the method which accesses additional methods to create a chord
 def chord
